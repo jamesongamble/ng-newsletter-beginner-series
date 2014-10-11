d935a8f81b2cb6ec158bf421c4e4ef0c6762a05f
jamesongamble <jamesongamble@gmail.com>
2014-10-10 17:13:50 -0700

    Works

--- js/beginner.js
+++ js/beginner.js
@@ -1,62 +1,58 @@
 var app = angular.module('myApp', []),
-    apiKey = 'MDExODQ2OTg4MDEzNzQ5OTM4Nzg5MzFiZA001',
+    apiKey = 'MDE3MDA3OTYyMDE0MTI5Nzk5MzcyYWQ0ZA001',
     nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-
-app.directive('nprLink', function() {
-  return {
-    restrict: 'EA',
-    require: ['^ngModel'],
-    replace: true,
-    scope: {
-      ngModel: '=',
-      play: '&'
-    },
-    templateUrl: 'views/nprListItem.html',
-    link: function(scope, ele, attr) {
-      scope.duration = scope.ngModel.audio[0].duration.$text;
-    }
-  }
+app.controller('MyController', function($scope) {
+  $scope.person = { name: "Ari Lerner" };
+  var updateClock = function() {
+    $scope.clock = new Date();
+  };
+  var timer = setInterval(function() {
+    $scope.$apply(updateClock);
+  }, 1000);
+  updateClock();
 });
 
 app.controller('PlayerController', function($scope, $http) {
-  var audio = document.createElement('audio');
-  $scope.audio = audio;
-
-  // audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-  // audio.play();
-
-  $scope.play = function(program) {
-    if ($scope.playing) audio.pause();
-    var url = program.audio[0].format.mp4.$text;
-    audio.src = url;
-    audio.play();
-    // Store the state of the player as playing
+  // Hidden our previous section's content
+  // construct our http request
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = '/media/npr.mp4';
+  $scope.play = function() {
+    $scope.audio.play();
     $scope.playing = true;
-  }
+  };
+  $scope.stop = function() {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop()
+    });
+  });
 
   $http({
     method: 'JSONP',
     url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
   }).success(function(data, status) {
-    // Now we have a list of the stories (data.list.story)
-    // in the data object that the NPR API 
-    // returns in JSON that looks like:
-    // data: { "list": {
-    //   "title": ...
-    //   "story": [
-    //     { "id": ...
-    //       "title": ...
+
     $scope.programs = data.list.story;
+    
+    $scope.play = function(program) {
+      if ($scope.playing) $scope.audio.pause();
+      console.log(program.audio[0].format.mp4.$text);
+      var url = program.audio[0].format.mp4.$text;
+  
+      $scope.streamAudio = document.createElement('audio');
+      $scope.streamAudio.src = url;
+      $scope.streamAudio.play();
+      $scope.playing = true;
+    }
   }).error(function(data, status) {
     // Some error occurred
   });
-
 });
 
-app.controller('RelatedController', function($scope) {
-});
 
-// Parent scope
-app.controller('FrameController', function($scope) {
-});
